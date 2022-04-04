253fcbf7d00c48ad28cd5b1237dab0653f99ba6a
namsic <52943870+namsic6460@users.noreply.github.com>
2022-04-04 21:58:46 +0900

    마인크래프트 dev 폴더 자동 복사 매크로 파일 추가

---
+++ copyToMinecraft.bat
@@ -0,0 +1,2 @@
+xcopy "C:\Users\user\Documents\GitHub\MCBE-KR\Cmd-Project\Cmd[B]" "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs\Cmd[B]" /E /y
+xcopy "C:\Users\user\Documents\GitHub\MCBE-KR\Cmd-Project\Cmd[R]" "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\Cmd[R]" /E /y
