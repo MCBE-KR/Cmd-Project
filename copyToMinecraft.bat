fce7639a39357637a8ad1670e634ea716510c411
namsic <52943870+namsic6460@users.noreply.github.com>
2022-04-04 23:01:30 +0900

    add /y

--- copyToMinecraft.bat
+++ copyToMinecraft.bat
@@ -1,2 +1,2 @@
-xcopy "%cd%\Cmd[B]" "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs" /E
-xcopy "%cd%\Cmd[R]" "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs" /E
+xcopy "%cd%\Cmd[B]" "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs" /E /y
+xcopy "%cd%\Cmd[R]" "C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs" /E /y
