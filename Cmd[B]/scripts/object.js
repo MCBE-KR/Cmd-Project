9a1147a3e2552d95b15968c8015827c659bc0de4
namsic <52943870+namsic6460@users.noreply.github.com>
2022-04-06 03:07:57 +0900

    파티클 속도 변경

--- Cmd[B]/scripts/object.js
+++ Cmd[B]/scripts/object.js
@@ -19,7 +19,7 @@ export class LineData {
 		let yDiff = src.y - dst.y
 		let zDiff = src.z - dst.z
 
-		this.dist = Math.sqrt((Math.abs(xDiff)**2)+ (Math.abs(yDiff)**2) + (Math.abs(zDiff)**2))
+		this.dist = Math.sqrt((Math.abs(xDiff) ** 2) + (Math.abs(yDiff) ** 2) + (Math.abs(zDiff) ** 2))
 		this.xDiff = (xDiff / this.dist)
 		this.yDiff = (yDiff / this.dist)
 		this.zDiff = (zDiff / this.dist)
